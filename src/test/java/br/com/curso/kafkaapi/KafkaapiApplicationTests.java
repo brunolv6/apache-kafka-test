8d0efe1b12590b4a205357c446f58e6e873582f0
Bruno Valle <brunolv6@gmail.com>
2021-07-09 18:22:06 -0300

    setup

---
+++ src/test/java/br/com/curso/kafkaapi/KafkaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.kafkaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
